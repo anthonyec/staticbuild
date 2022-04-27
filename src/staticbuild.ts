1e1b8afad6f1363a3f9b5ef320f7d1050f359c49
Anthony Cossins <anthonyecossins@gmail.com>
2022-04-27 15:41:20 +0100

    Add basic copy assets function

--- src/staticbuild.ts
+++ src/staticbuild.ts
@@ -5,6 +5,7 @@ import * as mustache from 'mustache';
 import { getUserConfig } from './config';
 import { getLayoutsFromFS } from './sources/getLayoutsFromFS';
 import { getFunctionsFromFS } from './sources/getFunctionsFromFS';
+import { recursiveReadDirectory } from './utils/fs';
 
 interface StaticBuildOptions {
   /** Specify an input folder containing website source files */
@@ -144,6 +145,26 @@ function getCollectionsFromPages(pages: Page[]): { [key: string]: Page[] } {
   }, {});
 }
 
+// TODO: Decide where this should live.
+function getAssetsFromPages(pages: Page[]): Asset[] {
+  const assets: Asset[] = [];
+
+  for (const page of pages) {
+    if (page.assets) {
+      assets.push(...page.assets);
+    }
+  }
+
+  return assets;
+}
+
+// TODO: Decide where this should live.
+async function copyAssets(assets: Asset[]) {
+  for await (const asset of assets) {
+    await fs.cp(asset.inputPath, asset.outputPath);
+  }
+}
+
 export default async function staticbuild(options: StaticBuildOptions) {
   console.time('setup');
   const config = getUserConfig(options.configPath);
@@ -152,12 +173,11 @@ export default async function staticbuild(options: StaticBuildOptions) {
 
   // const hooks = await getFunctionsFromFS(config.directories.hooks);
   // TODO: Is it possible for these functions to use a shared sourcing func?
+  // TODO: Add checks that paths exist.
   const functions = await getFunctionsFromFS(config.directories.functions);
   const data = await getFunctionsFromFS(config.directories.data);
   const layouts = await getLayoutsFromFS(config.directories.layouts);
   const partials = await getLayoutsFromFS(config.directories.partials);
-
-  // const data = await getDataFromFS(config.directories?.data);
   console.timeEnd('setup');
 
   console.time('source');
@@ -166,14 +186,14 @@ export default async function staticbuild(options: StaticBuildOptions) {
   const collections = getCollectionsFromPages(pages);
   console.timeEnd('source');
 
-  // console.time('copy');
-  // const assetsFromPages = await getAssetsFromPages(pages);
+  console.time('copy');
+  const assetsFromPages = getAssetsFromPages(pages);
   // const assetsToCopy = await getAssetsFilteredByChanges([...assets, ...assetsFromPages], changeList);
 
-  // await copyAssets(assetsToCopy);
-  // console.timeEnd('copy');
+  await copyAssets(assetsFromPages);
+  console.timeEnd('copy');
 
-  console.time('build');
+  console.time('render');
   await buildPages({
     pages,
     layouts,
@@ -182,6 +202,11 @@ export default async function staticbuild(options: StaticBuildOptions) {
     collections,
     data,
   });
-  console.timeEnd('build');
+  console.timeEnd('render');
+
+  // console.time('clean');
+  // await recursiveReadDirectory(options.inputDirectory);
+  // console.timeEnd('clean')
+
   // hooks.onPostBuild();
 }
